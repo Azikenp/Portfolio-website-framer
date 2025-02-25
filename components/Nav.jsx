cfefc1ea659f4e7b47d23d3817da5429334648d9
Azikenp <azikenp26@gmail.com>
2025-02-25 22:03:25 +0100

    new pages

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -16,15 +16,13 @@ const Nav = () => {
   console.log(pathname);
 
   return (
-    <nav>
+    <nav className="flex gap-8">
       {links.map((link, index) => {
         return (
           <Link
             href={link.path}
             key={index}
-            className={`${
-              link.path === pathname && "text-accent border-b-2 border-accent"
-            }`}
+            className={`${link.path === pathname && "text-accent border-b-2 border-accent"} capitalize font-medium hover:text-accent transition-all`}
           >
             {link.name}
           </Link>
