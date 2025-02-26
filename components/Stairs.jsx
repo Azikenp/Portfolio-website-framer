c53e06c6a56d49ecb83bedb5e85d055af5d35228
Azikenp <azikenp26@gmail.com>
2025-02-26 21:56:44 +0100

    animation variants

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -1,9 +1,20 @@
+import { animate, motion } from "framer-motion";
 
+//variants
+const stairAnimation = {
+  initial: {
+    top: "0%",
+  },
+  animate: {
+    top: "100%",
+  },
+  exit: {
+    top: ["100%", "0%"],
+  },
+};
 
 const Stairs = () => {
-  return (
-    <div>Stairs</div>
-  )
-}
+  return <div>Stairs</div>;
+};
 
-export default Stairs
+export default Stairs;
