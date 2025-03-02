496d2531cad8ceb6166d22cfd41e45cac88ecde3
Azikenp <azikenp26@gmail.com>
2025-03-02 16:30:30 +0100

    homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,41 @@
-export default function Home() {
-  return <main>homepage</main>;
-}
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
 
+export default function Home() {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm <br />{" "}
+              <span className="text-accent">Aziken Precious</span>
+            </h1>
+            <p className="max-w-[500p] mb-9 text-white/80">
+              I excel at crafting elegant digital experiences and I am
+              proficient in various programming languages and technologies.
+            </p>
+
+            {/* button and socials */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2 justify-center"
+              >
+                <span>Download CV</span>
+                <FiDownload />
+              </Button>
+              <div>socials</div>
+            </div>
+          </div>
+
+          {/* photo */}
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
+  );
+}
