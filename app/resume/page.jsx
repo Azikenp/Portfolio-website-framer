a17ceb376bdaa8836bf927b4253701a56ed9070c
Azikenp <azikenp26@gmail.com>
2025-03-03 17:40:51 +0100

    skills

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { FaCss3 } from "react-icons/fa";
+import { FaCss3, FaHtml5, FaJs, FaNodeJs, FaReact } from "react-icons/fa";
 
-import { SiTailwindcss } from "react-icons/si";
+import { SiNextdotjs, SiTailwindcss } from "react-icons/si";
 
 const about = {
   title: "About Me",
@@ -85,6 +85,31 @@ const education = {
       position: "Full Stack Web Development Bootcamp",
       duration: "2023",
     },
+    {
+      institution: "TOnline Course Platform",
+      position: "Full Stack Web Development Bootcamp",
+      duration: "2023",
+    },
+    {
+      institution: "TOnline Course Platform",
+      position: "Full Stack Web Development Bootcamp",
+      duration: "2023",
+    },
+  ],
+};
+
+const skills = {
+  title: "My Skills",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus iusto harum placeat necessitatibus.",
+  skillList: [
+    { icon: <FaHtml5 />, name: "html 5" },
+    { icon: <FaCss3 />, name: "css 3" },
+    { icon: <FaJs />, name: "javascript" },
+    { icon: <FaReact />, name: "react.js" },
+    { icon: <SiNextdotjs />, name: "next.js" },
+    { icon: <SiTailwindcss />, name: "tailwind css" },
+    { icon: <FaNodeJs />, name: "node.js" },
   ],
 };
 
