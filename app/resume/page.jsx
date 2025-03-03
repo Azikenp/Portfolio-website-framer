cb2138cfec1b27b50a6a7dcabd32fd5d01961bca
Azikenp <azikenp26@gmail.com>
2025-03-03 16:51:13 +0100

    about and experiance json

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,8 +1,66 @@
+"use client";
+
+import { FaCss3 } from "react-icons/fa";
+
+import { SiTailwindcss } from "react-icons/si";
+
+const about = {
+  title: "About Me",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus iusto harum placeat necessitatibus.",
+  info: [
+    { field: "Name", fieldValue: "Aziken Precious" },
+    { field: "Phone ", fieldValue: "(+234) 81 473 018 04" },
+    { field: "Experience", fieldValue: "2+ years" },
+    { field: "Skype", fieldValue: "aziken.01" },
+    { field: "Nationality", fieldValue: "Nigerian" },
+    { field: "Email", fieldValue: "azikenp26@gmail.com" },
+    { field: "Freelance", fieldValue: "Available" },
+    { field: "Langauges", fieldValue: "English, Igbo, Yoruba" },
+  ],
+};
+
+const experience = {
+  icon: "/assets/resume/badge.svg",
+  title: "My experience",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus iusto harum placeat necessitatibus.",
+  items: [
+    {
+      company: "Tech Solutions Inc",
+      position: "Full Stack Developer",
+      duration: "2022 - present",
+    },
+    {
+      company: "Ecommerce startup",
+      position: "Freelance Web Developer",
+      duration: "2020 - 2021",
+    },
+    {
+      company: "Web design studio",
+      position: "Frontend Developer Intern",
+      duration: "Summer 2021",
+    },
+    {
+      company: "Ecommerce startup",
+      position: "Freelance Web Developer",
+      duration: "2020 - 2021",
+    },
+    {
+      company: "Ecommerce startup",
+      position: "Freelance Web Developer",
+      duration: "2020 - 2021",
+    },
+    {
+      company: "Ecommerce startup",
+      position: "Freelance Web Developer",
+      duration: "2020 - 2021",
+    },
+  ],
+};
 
 const Resume = () => {
-  return (
-    <div>Resume</div>
-  )
-}
+  return <div>Resume</div>;
+};
 
-export default Resume
+export default Resume;
