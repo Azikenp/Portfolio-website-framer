0f44893c2b1082be8e6bfb48987c8ae1b825bcbf
Azikenp <azikenp26@gmail.com>
2025-03-04 21:34:22 +0100

    select

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -51,7 +51,34 @@ const Contact = () => {
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
           <div className="xl:h-[54%] order-2 xl:order-none">
-            <form>form</form>
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+              <h3 className="text-xl text-accent">Let's work together</h3>{" "}
+              <p className="text-white/60">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+                earum fugit consequatur rem voluptate. Quaerat.
+              </p>
+              {/* input */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <Input type="firstname" placeholder="First name" />
+                <Input type="lastname" placeholder="Last name" />
+                <Input type="email" placeholder="Email address" />
+                <Input type="phone" placeholder="Phone number" />
+              </div>
+              {/* select */}
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select a service" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select a service</SelectLabel>
+                    <SelectItem value="est">Web Development</SelectItem>
+                    <SelectItem value="cst">UI/UX Design</SelectItem>
+                    <SelectItem value="mst">Logo Design</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </form>
           </div>
           {/* info */}
           <div className="flex flex-1 items-center xl:justify-center order-1 xl:order-none mb-8 xl:mb-0">
