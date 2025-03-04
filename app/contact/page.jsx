440dcba6d2ec202f7e2bd618b2701ef89b7b2270
Azikenp <azikenp26@gmail.com>
2025-03-04 21:15:39 +0100

    form and info skeleton flexed

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -17,8 +17,50 @@ import {
 
 import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa";
 
+import { motion } from "framer-motion";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "(+234) 81 473 018 04",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "azikenp26@gmail.com",
+  },
+  {
+    icon: <FaMapMarkedAlt />,
+    title: "Address",
+    description: "4 eyita poultry",
+  },
+];
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          {/* form */}
+          <div className="xl:h-[54%] order-2 xl:order-none">
+            <form>form</form>
+          </div>
+          {/* info */}
+          <div className="flex flex-1 items-center xl:justify-center order-1 xl:order-none mb-8 xl:mb-0">
+            <div>info</div>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Contact;
